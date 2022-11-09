7760280dc9d92ec11d7c8e1d9e272407ac49bbbe
Thalia Levy <69092295+ThaliaLevy@users.noreply.github.com>
2022-11-09 13:56:38 -0300

    Delete loja-treino-gft directory

--- loja-treino-gft/src/main/java/gft/exception/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package gft.exception;
-
-public class EntityNotFoundException extends LojaException {
-	private static final long serialVersionUID = 1L;
-
-	public EntityNotFoundException(String message) {
-		super(message);
-	}
-}
