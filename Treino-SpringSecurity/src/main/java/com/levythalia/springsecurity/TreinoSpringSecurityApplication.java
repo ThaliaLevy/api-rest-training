ef1f3a16b94292299f94d07b3c71d146380865b5
ThaliaLevy <thalialevy@hotmail.com>
2022-11-11 15:27:40 -0300

    Novo projeto: treinar spring security

---
+++ Treino-SpringSecurity/src/main/java/com/levythalia/springsecurity/TreinoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.levythalia.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ Treino-SpringSecurity/src/test/java/com/levythalia/springsecurity/TreinoSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levythalia.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
